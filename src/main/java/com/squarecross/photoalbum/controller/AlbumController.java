9e6e1684a59471ae3f9a94cd5f46f1d096a1dac8
pulbak <hl2dop@naver.com>
2024-08-26 16:13:06 +0900

    create createAlbum

--- src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
+++ src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import com.squarecross.photoalbum.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/albums")
 public class AlbumController {
@@ -21,30 +23,33 @@ public class AlbumController {
         return new ResponseEntity<>(album, HttpStatus.OK);
     }
 
-    // Query String을 사용하는 메서드 http://localhost:8080/albums?albumId=2
-    @RequestMapping(method = RequestMethod.GET)  // @GetMapping사용가능
-    public ResponseEntity<AlbumDto> getAlbumByQuery(@RequestParam("albumId") final long albumId) {
-        AlbumDto album = albumService.getAlbum(albumId);
-        return new ResponseEntity<>(album, HttpStatus.OK);
-    }
+//    // Query String을 사용하는 메서드 http://localhost:8080/albums?albumId=2
+//    @RequestMapping(method = RequestMethod.GET)  // @GetMapping사용가능
+//   public ResponseEntity<AlbumDto> getAlbumByQuery(@RequestParam("albumId") final long albumId) {
+//        AlbumDto album = albumService.getAlbum(albumId);//       return new ResponseEntity<>(album, HttpStatus.OK);
+//    }
 
     // JSON body를 사용하는 메서드 (albumId만 받음) http://localhost:8080/albums body클릭후 json
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<AlbumDto> getAlbumByJson(@RequestBody AlbumIdRequest request) {
-        AlbumDto album = albumService.getAlbum(request.getAlbumId());
-        return new ResponseEntity<>(album, HttpStatus.OK);
+//    @RequestMapping(method = RequestMethod.POST)
+//    public ResponseEntity<AlbumDto> getAlbumByJson(@RequestBody AlbumIdRequest request) {
+//        AlbumDto album = albumService.getAlbum(request.getAlbumId());
+//        return new ResponseEntity<>(album, HttpStatus.OK);//   }
+    @RequestMapping(value = "",method = RequestMethod.POST) //url /httpl 메서드
+    public ResponseEntity<AlbumDto> createAlbum(@RequestBody final AlbumDto albumDto) throws IOException {  //입출력
+        AlbumDto saveAlbumDto = albumService.createAlbum(albumDto);
+        return new ResponseEntity<>(saveAlbumDto,HttpStatus.OK);
     }
 }
 
 // JSON 요청을 위한 간단한 클래스
-class AlbumIdRequest {
-    private long albumId;
+//class AlbumIdRequest {
+//    private long albumId;
 
-    public long getAlbumId() {
-        return albumId;
-    }
+//    public long getAlbumId() {
+//        return albumId;
+//    }
 
-    public void setAlbumId(long albumId) {
-        this.albumId = albumId;
-    }
-}
+//    public void setAlbumId(long albumId) {
+//        this.albumId = albumId;
+//    }
+//}
--- src/main/java/com/squarecross/photoalbum/service/AlbumService.java
+++ src/main/java/com/squarecross/photoalbum/service/AlbumService.java
@@ -8,7 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.squarecross.photoalbum.dto.AlbumDto;
 import com.squarecross.photoalbum.mapper.AlbumMapper;
 import com.squarecross.photoalbum.repository.PhotoRepository;
+
+import java.io.IOException;
 import java.util.Optional;
+
+import com.squarecross.photoalbum.Constants;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Service
 public class AlbumService {
@@ -27,5 +33,15 @@ public class AlbumService {
         else {
             throw new EntityNotFoundException(String.format("앨범 아이디 %d로 조회되지 않았습니다",albumId)); //없을시 에러 throw
         }
+    }
+    public AlbumDto createAlbum(AlbumDto albumDto) throws IOException {
+        Album album=AlbumMapper.convertToModel(albumDto);
+        this.albumRepository.save(album);
+        this.createAlbumDirectories(album);
+        return AlbumMapper.convertToDto(album);
+    }
+    private void createAlbumDirectories(Album album) throws IOException{
+        Files.createDirectories(Paths.get(Constants.PATH_PREFIX + "/photos/original/" + album.getAlbumId()));
+        Files.createDirectories(Paths.get(Constants.PATH_PREFIX + "/photos/thumb/" + album.getAlbumId()));
     }
 }
